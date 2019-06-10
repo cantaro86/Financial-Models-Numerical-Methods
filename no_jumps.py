704d5b14c0238e34f1d91e4293e32c4e0c51e785
canta86 <nicolacantarutti@gmail.com>
2019-06-10 10:09:13 +0200

    permanent update to merge

--- no_jumps.py
+++ no_jumps.py
@@ -1,59 +1,23 @@
-#! /usr/bin/python3 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 09:56:25 2019
+
+@author: cantaro86
+"""
+
 
 import time
-#import sys
+import sys
+
+print(sys.version)
 
 import numpy as np
 import numpy.matlib
 from copy import deepcopy
+import cost_utils as cost
 
-t=time.time()
-
-#%%
-def cost_f(x,y,cost_b,cost_s):
-    
-    cost = np.zeros( (len(x), len(y) ) )
-
-    for i in range(len(y)):
-        if y[i] <= 0 :
-            cost[:,i] = (1+cost_b) * y[i] * np.exp(x)
-        else:
-            cost[:,i] = (1-cost_s) * y[i] * np.exp(x)
-
-    return cost     # np.longfloat(np.transpose(cost))
-
-
-
-def cost_opt(x,y,cost_b,cost_s,K):
-
-    cost = np.zeros( (len(x),len(y)) )
-    
-    for i in range(len(x)):
-        for j in range(len(y)):
-    
-            if y[j] < 0 and np.exp(x[i]) <= K :
-                cost[i][j] = (1+cost_b) * y[j] * np.exp(x[i])
-            
-            elif y[j] >= 0 and np.exp(x[i]) <= K : 
-                cost[i][j] = (1-cost_s) * y[j] * np.exp(x[i])
-        
-            elif y[j]-1 >= 0 and np.exp(x[i]) > K :
-                cost[i][j] = ( (1-cost_s) * (y[j]-1) * np.exp(x[i]) ) + K
-        
-            elif y[j]-1 < 0 and np.exp(x[i]) > K :
-                cost[i][j] = ( (1+cost_b) * (y[j]-1) * np.exp(x[i]) ) + K
-
-    return np.longfloat(cost)
-    
-def cost_opt2(x,y,cost_b,cost_s,K):
-
-    cost = np.zeros( (len(x),len(y)) )
-    
-    for i in range(len(x)):
-        for j in range(len(y)):
-            cost[i,j] = cost_f([x[i]],[y[j]],cost_b,cost_s) if (np.exp(x[i]) <= K) else cost_f([x[i]],[y[j]-1],cost_b,cost_s) + K
-    return cost
-    
+t=time.time()    
     
 #%%    
 
@@ -64,20 +28,20 @@ sig = 0.25                # diffusion coefficient
 
 
 S0 = 15.0                  # current price
-x0 = np.log(S0)           # current log-price
+x0 = np.log(S0)            # current log-price
 
 K = 15.0                   # strike
-T = 1.0                  # year
+T = 1.0                    # year
 
-cost_b = 0.0             # (lambda in the paper) BUY cost  
-cost_s = 0.0             # (mu in the paper)  SELL cost
+cost_b = 0.0              # (lambda in the paper) BUY cost  
+cost_s = 0.0              # (mu in the paper)  SELL cost
 gamma = 0.01              # risk avversion coefficient
 
 #%%
 #def trans_cost_NJ( T=1, S0=15, K=15, r=0.1, mu=0.1, sig=0.25, cost=0.005, gamma=0.1, N=800  ):
 
-N = 2000          # time steps even!!!
-#cost1 = cost2 = cost
+N = 50          # time steps even!!!
+
         
 dt = T/N
 T_vec = np.linspace(0,T,N+1)
@@ -108,9 +72,9 @@ for TYPE in range(2):
 
     # Terminal
     if TYPE == 0:
-        Q = np.exp( np.longfloat( -gamma * np.longfloat( cost_f(x,y,cost_b,cost_s)) ))
+        Q = np.exp( np.longfloat( -gamma * np.longfloat( cost.no_opt(x,y,cost_b,cost_s)) ))
     else:
-        Q = np.exp( np.longfloat( -gamma * np.longfloat( cost_opt(x,y,cost_b,cost_s,K)) ))
+        Q = np.exp( np.longfloat( -gamma * np.longfloat( cost.writer(x,y,cost_b,cost_s,K)) ))
 
 
     for k in range(N-1,-1,-1):
@@ -145,3 +109,6 @@ elapsed=time.time()-t
 
 print("price: %.15f" %price )
 print("time: %.15f" %elapsed )
+
+
+#https://docs.scipy.org/doc/numpy/reference/generated/numpy.seterr.html
