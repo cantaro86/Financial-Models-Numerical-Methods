1655b5f3c788a73daefeb66c47ba0976f2918f7f
canta86 <nicolacantarutti@gmail.com>
2019-06-10 10:17:19 +0200

    add cost_utils

---
+++ cost_utils.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 09:56:25 2019
+
+@author: cantaro86
+"""
+
+import numpy as np
+
+#%%
+def no_opt(x,y,cost_b,cost_s):
+    
+    cost = np.zeros( (len(x), len(y) ) )
+
+    for i in range(len(y)):
+        if y[i] <= 0 :
+            cost[:,i] = (1+cost_b) * y[i] * np.exp(x)
+        else:
+            cost[:,i] = (1-cost_s) * y[i] * np.exp(x)
+
+    return cost     # np.longfloat(np.transpose(cost))
+
+
+
+def writer(x,y,cost_b,cost_s,K):
+
+    cost = np.zeros( (len(x),len(y)) )
+    
+    for i in range(len(x)):
+        for j in range(len(y)):
+    
+            if y[j] < 0 and np.exp(x[i]) <= K :
+                cost[i][j] = (1+cost_b) * y[j] * np.exp(x[i])
+            
+            elif y[j] >= 0 and np.exp(x[i]) <= K : 
+                cost[i][j] = (1-cost_s) * y[j] * np.exp(x[i])
+        
+            elif y[j]-1 >= 0 and np.exp(x[i]) > K :
+                cost[i][j] = ( (1-cost_s) * (y[j]-1) * np.exp(x[i]) ) + K
+        
+            elif y[j]-1 < 0 and np.exp(x[i]) > K :
+                cost[i][j] = ( (1+cost_b) * (y[j]-1) * np.exp(x[i]) ) + K
+
+    return cost  #np.longfloat(cost)
+    
+def cost_opt2(x,y,cost_b,cost_s,K):
+
+    cost = np.zeros( (len(x),len(y)) )
+    
+    for i in range(len(x)):
+        for j in range(len(y)):
+            cost[i,j] = cost_f([x[i]],[y[j]],cost_b,cost_s) if (np.exp(x[i]) <= K) else cost_f([x[i]],[y[j]-1],cost_b,cost_s) + K
+    return cost
