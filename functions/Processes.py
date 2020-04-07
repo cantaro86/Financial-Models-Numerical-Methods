9cebf3871edbafed96c3b07bf004dbc6f9fb8a2a
canta86 <nicolacantarutti@gmail.com>
2020-04-07 17:28:58 +0200

    new functions for VG and GARCH

--- functions/Processes.py
+++ functions/Processes.py
@@ -8,6 +8,10 @@ Created on Sat Jul 27 17:06:01 2019
 
 import numpy as np
 import scipy.stats as ss
+from functions.probabilities import VG_pdf
+from scipy.optimize import minimize
+from statsmodels.tools.numdiff import approx_hess
+import pandas as pd
 
 
 class Diffusion_process():
@@ -77,6 +81,7 @@ class VG_process():
     """
     def __init__(self, r=0.1, sigma=0.2, theta=-0.1, kappa=0.1):
         self.r = r
+        self.c = self.r
         self.theta = theta
         self.kappa = kappa
         if (sigma<0):
@@ -85,6 +90,7 @@ class VG_process():
             self.sigma = sigma
             
         # moments
+        self.mean = self.c + self.theta
         self.var = self.sigma**2 + self.theta**2 * self.kappa 
         self.skew = (2 * self.theta**3 * self.kappa**2 + 3*self.sigma**2 * self.theta * self.kappa) / (self.var**(1.5)) 
         self.kurt = ( 3*self.sigma**4 * self.kappa +12*self.sigma**2 * self.theta**2 \
@@ -98,8 +104,56 @@ class VG_process():
         VG = self.theta * G + self.sigma * np.sqrt(G) * Norm     # VG process at final time G
         S_T = S0 * np.exp( (self.r-w)*T + VG )                 # Martingale exponential VG       
         return S_T
+    
+    def path(self, T=1, N=10000, paths=1):
+        """
+        Creates Variance Gamma paths    
+        N = number of time points (time steps are N-1)
+        paths = number of generated paths
+        """
+        dt = T/(N-1)          # time interval        
+        X0 = np.zeros((paths,1))
+        G = ss.gamma( dt/self.kappa, scale=self.kappa).rvs( size=(paths,N-1) )     # The gamma RV
+        Norm = ss.norm.rvs(loc=0, scale=1, size=(paths,N-1))                       # The normal RV  
+        increments = self.c*dt + self.theta * G + self.sigma * np.sqrt(G) * Norm
+        X = np.concatenate((X0,increments), axis=1).cumsum(1)
+        return X
  
     
+    def fit_from_data(self, data, dt=1, method="Nelder-Mead"):
+        """
+        Fit the 4 parameters of the VG process using MM (method of moments), Nelder-Mead, L-BFGS-B.
+        data (array): datapoints
+        dt (float):     is the increment time
+        Returns (c,theta,sigma,kappa)
+        """
+        X = data
+        sigma_mm =  np.std(X) / np.sqrt(dt)                                
+        kappa_mm = dt * ss.kurtosis(X)/3                              
+        theta_mm = np.sqrt(dt) * ss.skew(X) * sigma_mm / (3*kappa_mm)   
+        c_mm     = np.mean(X)/dt - theta_mm
+        
+        def log_likely(x, data, T):
+            return (-1) * np.sum( np.log( VG_pdf(data, T, x[0], x[1], x[2], x[3]) ))
+        
+        if method=="L-BFGS-B":
+            if theta_mm<0:
+                result = minimize(log_likely, x0=[c_mm,theta_mm,sigma_mm,kappa_mm], method='L-BFGS-B', args=(X,dt), tol=1e-8,
+                     bounds=[[-0.5,0.5],[-0.6,-1e-15],[1e-15,1],[1e-15,2]])
+            else:
+                result = minimize(log_likely, x0=[c_mm,theta_mm,sigma_mm,kappa_mm], method='L-BFGS-B', args=(X,dt), tol=1e-8,
+                     bounds=[[-0.5,0.5],[1e-15,0.6],[1e-15,1],[1e-15,2]])
+            print(result.message)
+        elif method=="Nelder-Mead":
+            result = minimize(log_likely, x0=[c_mm,theta_mm,sigma_mm,kappa_mm], method='Nelder-Mead', args=(X,dt),
+                              options={'disp':False, 'maxfev':3000}, tol=1e-8)
+            print(result.message)
+        elif "MM":
+            self.c, self.theta, self.sigma, self.kappa = c_mm, theta_mm, sigma_mm, kappa_mm
+            return
+        self.c, self.theta, self.sigma, self.kappa = result.x
+         
+        
     
 class Heston_process():
     """
@@ -192,4 +246,122 @@ class NIG_process():
 
 
 
- 
+class GARCH():
+    """
+    Class for the GARCH(1,1) process. Variance process:
+        
+        V(t) = omega + alpha R^2(t-1) + beta V(t-1) 
+        
+        VL:  Unconditional variance >=0  
+        alpha: coefficient > 0   
+        beta:  coefficient > 0
+        gamma = 1 - alpha - beta
+        omega = gamma*VL
+    """
+    def __init__(self, VL=0.04, alpha=0.08, beta=0.9):
+        if (VL<0 or alpha<=0 or beta<=0):
+            raise ValueError("VL>=0, alpha>0 and beta>0")
+        else:
+            self.VL = VL
+            self.alpha = alpha
+            self.beta = beta
+        self.gamma = 1 - self.alpha - self.beta
+        self.omega = self.gamma * self.VL
+        
+    def path(self, N=1000):
+        """
+        Generates a path with N points. 
+        Returns the return process R and the variance process var
+        """
+        eps = ss.norm.rvs(loc=0, scale=1, size=N)
+        R = np.zeros_like(eps)
+        var = np.zeros_like(eps)
+        for i in range(N):
+            var[i] = self.omega + self.alpha*R[i-1]**2 + self.beta*var[i-1]
+            R[i] =  np.sqrt(var[i]) * eps[i]
+        return R, var
+   
+    
+    def fit_from_data(self, data, disp=True):
+        """
+        MLE estimator for the GARCH
+        """
+        # Automatic re-scaling:  1. the solver has problems with positive derivative in linesearch. 
+        #                        2. the log has overflows using small values 
+        n = np.floor(np.log10( np.abs(data.mean()) ))
+        R = data / 10**n
+    
+        # initial guesses
+        a0 = 0.05
+        b0 = 0.9
+        g0 = 1-a0-b0
+        w0 = g0*np.var(R)
+
+        # bounds and constraint
+        bounds = ((0, None), (0, 1), (0, 1))
+        def sum_small_1(x):
+            return 1-x[1]-x[2]
+        cons = ({"fun": sum_small_1, "type": "ineq"})  
+        
+        def log_likely(x):
+            var = R[0]**2             # initial variance
+            N = len(R)
+            log_lik=0
+            for i in range(1,N):
+                var = x[0] + x[1]*R[i-1]**2 + x[2]*var    # variance update 
+                log_lik += -np.log(var) - ( R[i]**2 / var )  
+            return (-1)*log_lik
+    
+        result = minimize(log_likely, x0=[w0,a0,b0], method='SLSQP', bounds=bounds,
+                          constraints=cons, tol=1e-8, options={"maxiter":150})
+        print(result.message)
+        self.omega = result.x[0] * 10**(2*n)  
+        self.alpha, self.beta = result.x[1:]
+        self.gamma = 1-self.alpha-self.beta
+        self.VL = self.omega / self.gamma
+
+        if disp==True:            
+            hess = approx_hess(result.x, log_likely )    # hessian by finite differences
+            se = np.sqrt(np.diag(np.linalg.inv(hess) ) ) # standard error
+            cv = ss.norm.ppf(1.0 - 0.05 / 2.0)           # alpha=0.05 
+            p_val = ss.norm.sf(np.abs( result.x / se))   # survival function
+            
+            df = pd.DataFrame(index=["omega", "alpha", "beta"])
+            df["Params"] = result.x
+            df["SE"] = se 
+            df["P-val"] = p_val 
+            df["95% CI lower"] = result.x - cv * se
+            df["95% CI upper"] = result.x + cv * se
+            df.loc["omega", ["Params", "SE", "95% CI lower", "95% CI upper"]] *= 10**(2*n)
+            print(df)
+    
+    
+    def log_likelihood(self, R, last_var=True):
+        """
+        Computes the log-likelihood and optionally returns the last value of the variance
+        """
+        var = R[0]**2             # initial variance
+        N = len(R)
+        log_lik=0
+        log_2pi = np.log(2 * np.pi)
+        for i in range(1,N):
+            var = self.omega + self.alpha*R[i-1]**2 + self.beta*var    # variance update 
+            log_lik += 0.5 * ( -log_2pi -np.log(var) - ( R[i]**2 / var )  )
+        if last_var==True:  
+            return log_lik, var
+        else:
+            return log_lik
+        
+    def generate_var(self, R, R0, var0):
+        """
+        generate the variance process.
+        R (array): return array
+        R0: initial value of the returns
+        var0: initial value of the variance
+        """
+        N = len(R)
+        var = np.zeros(N)
+        var[0] = self.omega + self.alpha*(R0**2) + self.beta*var0
+        for i in range(1,N):
+            var[i] = self.omega + self.alpha*R[i-1]**2 + self.beta*var[i-1]
+        return var
--- functions/probabilities.py
+++ functions/probabilities.py
@@ -10,6 +10,8 @@ import numpy as np
 from scipy.integrate import quad
 from functools import partial
 from functions.CF import cf_Heston_good
+import scipy.special as scps
+from math import factorial
 
 
 def Q1(k, cf, right_lim):
@@ -55,5 +57,34 @@ def Heston_pdf(i, t, v0, mu, theta, sigma, kappa, rho):
     return Gil_Pelaez_pdf(i, cf_H_b_good, np.inf)
 
 
+def VG_pdf(x,T,c,theta,sigma,kappa):
+    """
+    Variance Gamma density function
+    """
+    return 2 * np.exp( theta*(x-c) / sigma**2 ) / (kappa**(T/kappa) * np.sqrt(2*np.pi) *\
+        sigma * scps.gamma(T/kappa) ) * \
+        ((x-c)**2 / (2*sigma**2 /kappa +theta**2) )**( T/(2*kappa)-1/4) * \
+        scps.kv(T/kappa-1/2, sigma**(-2) * np.sqrt((x-c)**2 * (2*sigma**2/kappa + theta**2)))
 
 
+
+def Merton_pdf(x, T, mu, sig, lam, muJ, sigJ):
+    """
+    Merton density function
+    """
+    tot = 0
+    for k in range(20):
+        tot += (lam*T)**k * np.exp(-(x-mu*T-k*muJ)**2/( 2*(T*sig**2+k*sigJ**2) ) ) \
+                / (factorial(k) * np.sqrt(2*np.pi * (sig**2*T+k*sigJ**2) ) )  
+    return np.exp(-lam*T) * tot
+
+
+def NIG_pdf(x,T,c,theta,sigma,kappa):
+    """
+    Merton density function
+    """
+    A = theta/(sigma**2)
+    B = np.sqrt( theta**2 + sigma**2/kappa ) / sigma**2
+    C = T/np.pi * np.exp(T/kappa) * np.sqrt( theta**2/(kappa*sigma**2) + 1/kappa**2 )
+    return C * np.exp(A*(x-c*T)) * scps.kv(1, B*np.sqrt((x-c*T)**2 + T**2 * sigma**2/kappa ) ) \
+                / np.sqrt((x-c*T)**2 + T**2 * sigma**2/kappa )
