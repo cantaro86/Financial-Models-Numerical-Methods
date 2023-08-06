3ca770e27c67d3e86d4abc374824829d98435b3f
Cantaro86 <nicolacantarutti@gmail.com>
2023-08-06 16:47:12 +0200

    __all__ in init + functions to FMNM + bug fixing

--- src/FMNM/__init__.py
+++ src/FMNM/__init__.py
@@ -6,4 +6,24 @@ Created on Sat Aug 5 16:35:17 2023
 @author: cantaro86
 """
 
+
+__all__ = [
+    "cython",
+    "BS_pricer",
+    "CF",
+    "cost_utils",
+    "FFT",
+    "Heston_pricer",
+    "Kalman_filter",
+    "Merton_pricer",
+    "NIG_pricer",
+    "Parameters",
+    "portfolio_optimization",
+    "probabilities",
+    "Processes",
+    "Solvers",
+    "TC_pricer",
+    "VG_pricer",
+]
+
 from FMNM import *
